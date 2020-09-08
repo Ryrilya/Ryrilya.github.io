dd24135834a51d000153c0cd11c0a542b414f707
federico.chilese <federicochilese@gmail.com>
2020-09-08 20:22:54 +0200

    Updated

--- js/aframe-components.js
+++ js/aframe-components.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent('out-of-focus-shape', {
+AFRAME.registerComponent('out-of-focus', {
   init: function () {
     var marker = this.el;
     var element = marker.children[0];
@@ -24,30 +24,3 @@ AFRAME.registerComponent('out-of-focus-shape', {
     });
   }
 });
-
-AFRAME.registerComponent('out-of-focus-gltf', {
-  init: function () {
-    var marker = this.el;
-    var element = marker.children[0];
-    var contentMarkerLost = document.querySelector("#content-markerLost");
-
-    marker.setAttribute('emitevents', 'true');
-
-    // If the marker is found
-    marker.addEventListener('markerFound', function () {
-      // Hide the centered content
-      contentMarkerLost.setAttribute("visible", false);
-    });
-    marker.addEventListener('markerLost', function () {
-      // Take the marker's content and set the same values to the content hidden in the center of the screen
-      let model = element.getAttribute('gltf-model');
-      let scale = element.getAttribute('scale');
-
-      contentMarkerLost.setAttribute("visible", true); // Make visible the centered content
-      contentMarkerLost.setAttribute("gltf-model", model);
-      contentMarkerLost.setAttribute("scale", scale);
-      contentMarkerLost.setAttribute("rotation", { x: 0, y: 0, z: 0 })
-    });
-  }
-});
-

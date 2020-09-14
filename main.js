9a23fc8825e9ea924afd4a4f90898955182da7e8
federico.chilese <federicochilese@gmail.com>
2020-09-14 17:55:07 +0200

    Updated index.html

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-var button = document.querySelector("#calculate-distance-btn");
-var text = document.querySelector("#distance-text");
-
-button.addEventListener("click", () => {
-  const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
-  text.value = distanceMsg;
-  console.log(distanceMsg);
-});
+console.dir(document.querySelector('[gps-camera]').getAttribute('alert'));

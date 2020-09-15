2444454f1c2bd5b367b9177c296f7c30e3202d4e
federico.chilese <federicochilese@gmail.com>
2020-09-15 17:54:50 +0200

    Changed project
    Changed project from Location Based GPS to Object Manipulation

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
-# Location-Based GPS Demo | AR.js + A-Frame (v0.0.1)
-
+# User input manipulation in A-Frame | AR.js - (v0.0.1)
 ### DESCRIPTION 
-This is a demo where i try to develop a web application using [AR.js](https://ar-js-org.github.io/AR.js/) and [A-Frame](https://aframe.io/). The current attempt is to dispaly a model only when the user device is in a specific location (GPS based).
-**E.g** [This demo result made by Nicolò Carpignoli](https://miro.medium.com/max/270/1*GTnNxf_j4b_5AJElAKWF3w.gif)
+The purpose of this demo is to make possible to let the user manipulate the object shown by a marker (zoom in, zoom out, rotate it, and so on...).
 
 ### TECHNOLOGIES
 * [A-Frame](https://aframe.io/) 
 * [AR.js](https://ar-js-org.github.io/AR.js/)
+* [Hammer.js](https://hammerjs.github.io/)
+
+# HOW TO USE THE APPLICATION
+The application works in this way: when you don't show a Hiro Marker to the camera, then nothing is displayed. So you have to do that in order to see something appearing on the screen. But what happens if the marker is not being tracked by the software anymore? Normally, nothing happens exept that the object disappears. 
+Though, in this application, when the marker is shown to the camera, the object appears in the middle of the screen! Moreover you have the possibility to:
+### ROTATE :arrows_counterclockwise:
+* :computer: **Desktop:** dragging the object around
+* :iphone: **Mobile:** panning on the screen
+### ZOOM IN/ZOOM OUT :mag:
+* :computer: **Desktop:** either +/- buttons on the up-right corner of the screen and scroll wheel
+* :iphone: **Mobile:** pinch the object
 
 # NOTES
 * Not responsive so far
 
 
 Have fun and feel free to take the code!
-Share your projects with me on [Twitter](https://twitter.com/Rierilya)
-
+Share your projects with me on [Twitter](https://twitter.com/Ryrilya_) 
---
+++ js/main.js
@@ -0,0 +1,71 @@
+var body = document.querySelector("body");
+var showContentBtn = document.querySelector("#show-content-btn");
+var markerLostEntity = document.querySelector("#content-markerLost");
+var modelControlsBtns = document.querySelector("#model-controls-btns");
+
+// Hammer.js enters in action!
+var hammertime = new Hammer(body);
+var pinch = new Hammer.Pinch();
+hammertime.add(pinch);
+
+// >> PAN
+hammertime.on('pan', (ev) => {
+  body.style.cursor = "-webkit-grab";   // Sets the cursor to a grabbing hand
+
+  let rotation = markerLostEntity.getAttribute("rotation");
+
+  // Rotates the object based on user input direction
+  switch (ev.direction) {
+    case 2:
+      rotation.y += 4;
+      break;
+    case 4:
+      rotation.y -= 4;
+      break;
+    case 8:
+      rotation.x += 4;
+      break;
+    case 16:
+      rotation.x -= 4;
+      break;
+    default:
+      break;
+  }
+
+  markerLostEntity.setAttribute("rotation", rotation);
+});
+
+// >> PINCH
+hammertime.on("pinch", (ev) => {
+  let scale = { x: ev.scale, y: ev.scale, z: ev.scale };
+  module.setAttribute("scale", scale);
+});
+
+
+showContentBtn.addEventListener("click", () => {
+  markerLostEntity.setAttribute("visible", false);  // Close the content in the middle of the screen
+  showContentBtn.style.display = "none";            // Close the "Hide Content" button
+  modelControlsBtns.style.display = "none";         // Close zoom buttons
+  body.style.cursor = "initial";                    // Sets the cursor to default style
+});
+
+// Event fired when the user uses the scrollwheel on his mouse
+body.addEventListener("wheel", (event) => {
+  // Change the size of the entity based on scrolling direction
+
+  let y = event.deltaY; // takes the event value
+  let scale = markerLostEntity.getAttribute("scale"); // takes the entity's scale
+
+
+  if (y > 0) {
+    // If the user scrolls down then decrease the entity's size
+    scale.x -= 0.01;
+    scale.y -= 0.01;
+    scale.z -= 0.01;
+  } else {
+    // If the user scrolls down then increase the entity's size
+    scale.x += 0.01;
+    scale.y += 0.01;
+    scale.z += 0.01;
+  }
+});

4179726ef4bf7711dccb7fe07a49220ad28dfa21
Federico Chilese <federiochilese@gmail.com>
2020-09-25 12:13:21 +0200

    Modified model's GPS location

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ function staticLoadPlaces() {
     {
       name: "Magnemite",
       location: {
-        lat: 45.6732267,
-        lng: 11.9589154,
+        lat: 45.6730203,
+        lng: 11.9589757,
       },
     },
   ];
@@ -29,12 +29,13 @@ function renderPlaces(places) {
   places.forEach(function (place) {
     var latitude = place.location.lat;
     var longitude = place.location.lng;
+
     model = document.createElement("a-entity");
+    model.setAttribute("id", "model");
     model.setAttribute(
       "gps-entity-place",
       `latitude: ${latitude}; longitude: ${longitude};`
     );
-    model.setAttribute("id", "model");
     model.setAttribute("gltf-model", "./assets/magnemite/scene.gltf");
     model.setAttribute("rotation", "0 0 0");
     model.setAttribute("position", "0 0 -30");
@@ -50,11 +51,10 @@ function renderPlaces(places) {
 const latitude = document.querySelector("#latitude");
 const longitude = document.querySelector("#longitude");
 const distanceMsg = document.querySelector("#distance-msg");
-function getCoordinates() {
+function showCoordinatesRealTime() {
   navigator.geolocation.getCurrentPosition((location) => {
     latitude.value = location.coords.latitude;
     longitude.value = location.coords.longitude;
-    // console.log(model.getAttribute("distancemsg"));
 
     const distanceMsgValue = model.getAttribute("distancemsg");
     distanceMsgValue == null
@@ -63,7 +63,7 @@ function getCoordinates() {
   });
 }
 
-setInterval(getCoordinates, 1000);
+setInterval(showCoordinatesRealTime, 1000);
 
 const btn = document.querySelector("#center-btn");
 btn.addEventListener("click", () => {

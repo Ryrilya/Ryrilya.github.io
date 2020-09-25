442e7bf3759f9034ddb2e3990f53abee31411a01
Federico Chilese <58114714+Ryrilya@users.noreply.github.com>
2020-09-25 15:38:11 +0200

    Update main.js
    Updated placeText.scale from 15 15 15 to 5 5 5

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ window.onload = function () {
             "latitude: " + latitude + "; longitude: " + longitude
           );
           placeText.setAttribute("title", place.name);
-          placeText.setAttribute("scale", "15 15 15");
+          placeText.setAttribute("scale", "5 5 5");
           placeText.addEventListener("loaded", function () {
             window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
             console.log("Place text loaded");

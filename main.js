2752411ec62155245bdc426e7ed9220423a4546e
Federico Chilese <federiochilese@gmail.com>
2020-09-25 11:56:03 +0200

    Removed orientation feature. Added Distance message

--- main.js
+++ main.js
@@ -21,38 +21,45 @@ function staticLoadPlaces() {
     },
   ];
 }
+
+var model = null;
+
 function renderPlaces(places) {
   var scene = document.querySelector("a-scene");
   places.forEach(function (place) {
     var latitude = place.location.lat;
     var longitude = place.location.lng;
-    var model = document.createElement("a-entity");
+    model = document.createElement("a-entity");
     model.setAttribute(
       "gps-entity-place",
-      "latitude: " + latitude + ", longitude: " + longitude
+      `latitude: ${latitude}; longitude: ${longitude};`
     );
     model.setAttribute("id", "model");
     model.setAttribute("gltf-model", "./assets/magnemite/scene.gltf");
     model.setAttribute("rotation", "0 0 0");
-//     model.setAttribute("animation-mixer", "");
+    model.setAttribute("position", "0 0 -30");
+    model.setAttribute("animation-mixer", "");
     model.setAttribute("scale", "0.5 0.5 0.5");
     model.addEventListener("loaded", function () {
       window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
     });
     scene.appendChild(model);
-    model.setAttribute("position", "0 0 -30");
   });
 }
 
 const latitude = document.querySelector("#latitude");
 const longitude = document.querySelector("#longitude");
+const distanceMsg = document.querySelector("#distance-msg");
 function getCoordinates() {
   navigator.geolocation.getCurrentPosition((location) => {
     latitude.value = location.coords.latitude;
     longitude.value = location.coords.longitude;
+    // console.log(model.getAttribute("distancemsg"));
 
-    // console.log("Latitude" + location.coords.latitude);
-    // console.log("Longitude" + location.coords.longitude);
+    const distanceMsgValue = model.getAttribute("distancemsg");
+    distanceMsgValue == null
+      ? (distanceMsg.value = "Undefined")
+      : (distanceMsg.value = distanceMsgValue);
   });
 }
 

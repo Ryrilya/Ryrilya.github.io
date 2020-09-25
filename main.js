b9f166a6fe06e09fb2c8129abda200fbb277fab6
Federico Chilese <federiochilese@gmail.com>
2020-09-25 15:58:07 +0200

    Used Typescript

--- main.js
+++ main.js
@@ -1,63 +1,52 @@
+"use strict";
+exports.__esModule = true;
+// getting places from APIs
 function loadPlaces(position) {
-  var params = {
-    radius: 300,
-    clientId: "SAAG2KJSSXRU45K0OHRPMRMFBGNHHC1F45D5JAXYHTZ0HJUR",
-    clientSecret: "3SPETMYIWUUT1R1CB0XBUJGCFYMHAOVVODOPNMVCKPVQ0AFD",
-    version: "20300101",
-    limit: 30,
-  };
-  var corsProxy = "https://cors-anywhere.herokuapp.com/";
-  // Foursquare API
-  const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-        &ll=${position.latitude},${position.longitude}
-        &radius=${params.radius}
-        &client_id=${params.clientId}
-        &client_secret=${params.clientSecret}
-        &limit=30 
-        &v=${params.version}`;
-  return fetch(endpoint)
-    .then(function (res) {
-      return res.json().then(function (resp) {
-        return resp.response.venues;
-      });
-    })
-    ["catch"](function (err) {
-      console.error("Error with places API", err);
+    var params = {
+        radius: 300,
+        clientId: '<your-client-id>',
+        clientSecret: '<your-client-secret>',
+        version: '20300101'
+    };
+    // CORS Proxy to avoid CORS problems
+    var corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    // Foursquare API (limit param: number of maximum places to fetch)
+    var endpoint = corsProxy + "https://api.foursquare.com/v2/venues/search?intent=checkin\n      &ll=" + position.latitude + "," + position.longitude + "\n      &radius=" + params.radius + "\n      &client_id=" + params.clientId + "\n      &client_secret=" + params.clientSecret + "\n      &limit=30 \n      &v=" + params.version;
+    return fetch(endpoint)
+        .then(function (res) {
+        return res.json()
+            .then(function (resp) {
+            return resp.response.venues;
+        });
+    })["catch"](function (err) {
+        console.error('Error with places API', err);
     });
 }
+;
 window.onload = function () {
-  var scene = document.querySelector("a-scene");
-  // get current user location
-  return navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // then use it to load remote APIs some places nearby
-      loadPlaces(position.coords).then(function (places) {
-        places.forEach(function (place) {
-          var latitude = place.location.lat;
-          var longitude = place.location.lng;
-          // add place name
-          var placeText = document.createElement("a-link");
-          placeText.setAttribute(
-            "gps-entity-place",
-            "latitude: " + latitude + "; longitude: " + longitude
-          );
-          placeText.setAttribute("title", place.name);
-          placeText.setAttribute("scale", "15 15 15");
-          placeText.addEventListener("loaded", function () {
-            window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
-            console.log("Place text loaded");
-          });
-          scene.appendChild(placeText);
+    var scene = document.querySelector('a-scene');
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then(function (places) {
+            places.forEach(function (place) {
+                var latitude = place.location.lat;
+                var longitude = place.location.lng;
+                // add place name
+                var placeText = document.createElement('a-link');
+                placeText.setAttribute('gps-entity-place', "latitude: " + latitude + "; longitude: " + longitude + ";");
+                placeText.setAttribute('title', place.name);
+                placeText.setAttribute('scale', '5 5 5');
+                placeText.addEventListener('loaded', function () {
+                    window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'));
+                });
+                scene.appendChild(placeText);
+            });
         });
-      });
-    },
-    function (err) {
-      return console.error("Error in retrieving position", err);
-    },
-    {
-      enableHighAccuracy: true,
-      maximumAge: 0,
-      timeout: 27000,
-    }
-  );
+    }, function (err) { return console.error('Error in retrieving position', err); }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 27000
+    });
 };

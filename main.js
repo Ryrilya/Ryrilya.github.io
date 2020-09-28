86682d5ca7cef0f4201b576dd6c297be7f9f946d
Federico Chilese <federiochilese@gmail.com>
2020-09-28 14:22:45 +0200

    Changed project to Gesture Demo

--- main.js
+++ main.js
@@ -1,71 +1,18 @@
-//import { Place } from './models/place';
-// getting places from APIs
-function loadPlaces(position) {
-  var params = {
-    radius: 300,
-    clientId: "SAAG2KJSSXRU45K0OHRPMRMFBGNHHC1F45D5JAXYHTZ0HJUR",
-    clientSecret: "3SPETMYIWUUT1R1CB0XBUJGCFYMHAOVVODOPNMVCKPVQ0AFD",
-    version: "20300101",
-  };
-  // CORS Proxy to avoid CORS problems
-  var corsProxy = "https://cors-anywhere.herokuapp.com/";
-  // Foursquare API (limit param: number of maximum places to fetch)
-  var endpoint =
-    corsProxy +
-    "https://api.foursquare.com/v2/venues/search?intent=checkin&ll=" +
-    position.latitude +
-    "," +
-    position.longitude +
-    "&radius=" +
-    params.radius +
-    "&client_id=" +
-    params.clientId +
-    "&client_secret=" +
-    params.clientSecret +
-    "&limit=30&v=" +
-    params.version;
-  return fetch(endpoint)
-    .then(function (res) {
-      return res.json().then(function (resp) {
-        return resp.response.venues;
-      });
-    })
-    ["catch"](function (err) {
-      console.error("Error with places API", err);
-    });
+const model = document.querySelector("#model");
+
+model.addEventListener("markerFound", (e) => {
+  isMarkerVisible = true;
+});
+
+model.addEventListener("markerLost", (e) => {
+  isMarkerVisible = false;
+});
+model.addEventListener("onefingermove", handleRotation);
+
+function handleRotation(event) {
+  if (isMarkerVisible) {
+    el.object3D.rotation.y += event.detail.positionChange.x * rotationFactor;
+
+    el.object3D.rotation.x += event.detail.positionChange.y * rotationFactor;
+  }
 }
-window.onload = function () {
-  var scene = document.querySelector("a-scene");
-  // first get current user location
-  return navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // than use it to load from remote APIs some places nearby
-      loadPlaces(position.coords).then(function (places) {
-        places.forEach(function (place) {
-          var latitude = place.location.lat;
-          var longitude = place.location.lng;
-          // add place name
-          var placeText = document.createElement("a-link");
-          placeText.setAttribute(
-            "gps-entity-place",
-            "latitude: " + latitude + "; longitude: " + longitude + ";"
-          );
-          placeText.setAttribute("title", place.name);
-          placeText.setAttribute("scale", "5 5 5");
-          placeText.addEventListener("loaded", function () {
-            window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
-          });
-          scene.appendChild(placeText);
-        });
-      });
-    },
-    function (err) {
-      return console.error("Error in retrieving position", err);
-    },
-    {
-      enableHighAccuracy: true,
-      maximumAge: 0,
-      timeout: 27000,
-    }
-  );
-};

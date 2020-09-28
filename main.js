a30a4529277e4edc9138dfdbcaadb57240f9bae5
Federico Chilese <federiochilese@gmail.com>
2020-09-28 14:34:37 +0200

    Updated index.html

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ model.addEventListener("onefingermove", handleRotation);
 
 function handleRotation(event) {
   if (isMarkerVisible) {
-    el.object3D.rotation.y += event.detail.positionChange.x * rotationFactor;
+    model.object3D.rotation.y += event.detail.positionChange.x * rotationFactor;
 
-    el.object3D.rotation.x += event.detail.positionChange.y * rotationFactor;
+    model.object3D.rotation.x += event.detail.positionChange.y * rotationFactor;
   }
 }
